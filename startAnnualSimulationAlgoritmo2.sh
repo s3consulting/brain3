e1a33343b3e6bc8fc22a8986976e7a85dcd44b25
cristianocimino <cristiano.cimino@gmail.com>
2023-05-02 16:26:55 +0200

    aggiunti quattro script

---
+++ startAnnualSimulationAlgoritmo2.sh
@@ -0,0 +1,1 @@
+java -cp ./target/brain3Simulator-jar-with-dependencies.jar time.AnnualSimulation_ALGORITMO2  /Users/cristianocimino/NetBeansProjects/generic-graph/GRAPHS /Users/cristianocimino/NetBeansProjects/generic-graph/SIMULATION_ANNUAL
